e7104a0ebb01d06dfcdf735c97eadb3dcd35d8f6
Kevin Earls <kearls@kevinearls.com>
2018-12-05 15:25:54 +0100

    Minor cleanup

--- alchemical-reduction-day5/src/main/java/com/kevinearls/alchemical/Polymer.java
+++ alchemical-reduction-day5/src/main/java/com/kevinearls/alchemical/Polymer.java
@@ -7,7 +7,6 @@ import java.util.List;
 public class Polymer {
     public int react(String input) {
         List<String> content = new LinkedList<>(Arrays.asList(input.split("")));
-//        System.out.println("Size " + content.size());
 
         int position = 0;
         String current = "";
@@ -17,10 +16,8 @@ public class Polymer {
             next = content.get(position + 1);
 
             if ((!current.equals(next)) && (current.equalsIgnoreCase(next))) {
-                //System.out.println("Removing " + current + next + " at position " + position);
                 content.remove(position);
                 content.remove(position);
-                //System.out.println("After remove [" + content.toString() + "]");
                 if (position > 0) {
                     position--;
                 }
@@ -35,17 +32,13 @@ public class Polymer {
     // For part 2
     public int improvedReact(String input) {
         int shortestLength = Integer.MAX_VALUE;
-        String shortestLetter;
 
         for (char c = 'a'; c <= 'z'; c++ ) {
-            //System.out.println(c);
             String target = "[" + c + String.valueOf(c).toUpperCase() + "]";
-            //System.out.println(target);
             String strippedInput = input.replaceAll(target, "");
             int length = react(strippedInput);
             if (length < shortestLength) {
                 shortestLength = length;
-                shortestLetter = String.valueOf(c);
             }
         }
 
